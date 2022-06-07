fc1bc3f22de59e98c3bf44471753c991788c0855
ivana.ste <stevanovic.sw69.2019@uns.ac.rs>
2022-06-08 00:11:10 +0200

    Brisanje postojece stanice

--- SrbijaVoz/SrbijaVoz/managerPages/StationsPage.xaml.cs
+++ SrbijaVoz/SrbijaVoz/managerPages/StationsPage.xaml.cs
@@ -24,17 +24,21 @@ namespace SrbijaVoz.managerPages
 {
     public partial class StationsPage : Page
     {
-        private List<Station> Stations;
+        public List<Station> Stations;
+
+        public bool IsSelected = true;
 
         public StationsPage(List<Station> stations)
         {
-            InitializeComponent();
             Stations = stations;
+            InitializeComponent();
             StationsNetworkMap.Focus();
-            ShowStationsOnMap();
+            DrawStationsOnMap();
+            StationsList.ItemsSource = Stations;
+
         }
 
-        private void ShowStationsOnMap()
+        private void DrawStationsOnMap()
         {
             foreach(Station station in Stations)
             {
@@ -44,27 +48,68 @@ namespace SrbijaVoz.managerPages
 
         private void AddStationOnMap(Station station)
         {
+            Pushpin pin = CreateStationPin(station);
+            StationsNetworkMap.Children.Add(pin);
+        }
+
+        private static Pushpin CreateStationPin(Station station)
+        {
             Pushpin pin = new Pushpin();
             ToolTip toolTip = new ToolTip();
             pin.Content = station.Id;
             toolTip.Content = "Stanica " + station.Name;
             toolTip.Cursor = Cursors.Arrow;
             pin.ToolTip = toolTip;
-            
             pin.Location = new Location(station.Location);
-            StationsNetworkMap.Children.Add(pin);
+            return pin;
         }
+
 
         public void AddNewStation_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            DragDrop.DoDragDrop(PinIcon, PinIcon, DragDropEffects.Move);
+            /*Point dropPosition = e.GetPosition(StationsNetworkMap);
+            Canvas.SetLeft(PinIcon, dropPosition.X);
+            Canvas.SetTop(PinIcon, dropPosition.X);*/
+            DragDrop.DoDragDrop(PinIcon, "add", DragDropEffects.Move);
         }
 
         private void StationsNetworkMap_Drop(object sender, DragEventArgs e)
         {
             Point dropPosition = e.GetPosition(StationsNetworkMap);
-            _ = AddStation(dropPosition);
-           
+            if (e.Data.GetData(DataFormats.StringFormat).ToString() == "add")
+            {
+                _ = AddStation(dropPosition);
+            } else
+            {
+                DeleteStation(dropPosition);
+            }           
+        }
+
+        private void DeleteStation(Point dropPosition)
+        {
+            Location location = StationsNetworkMap.ViewportPointToLocation(dropPosition);
+            Station station = FindStationByLocation(location);
+            if (station != null)
+            {
+                Pushpin pin = CreateStationPin(station);
+                Stations.Remove(station);
+                StationsList.Items.Refresh();
+                StationsNetworkMap.Children.Clear();
+                DrawStationsOnMap();
+
+            }
+        }
+
+        private Station? FindStationByLocation(Location location)
+        {
+            foreach(Station station in Stations)
+            {
+                Trace.WriteLine(station.Name);
+                Trace.WriteLine("Latitude: " + (location.Latitude - station.Location.Latitude).ToString());
+                Trace.WriteLine("Longitude: " + (location.Longitude - station.Location.Longitude).ToString());
+                if ((Math.Abs(station.Location.Latitude - location.Latitude) <= 0.001) && (Math.Abs(station.Location.Longitude - location.Longitude) <= 0.001)) return station;
+            }
+            return null;
         }
 
         private async Task<bool> AddStation(Point dropPosition)
@@ -81,10 +126,52 @@ namespace SrbijaVoz.managerPages
                 int newId = Stations.Last().Id + 1;
                 Station newStation = new(newId, viewModel.MessageBoxInput, pinLocation);
                 Stations.Add(newStation);
+                StationsList.Items.Refresh();
                 AddStationOnMap(newStation);
                 return true;
             }
             return false;
+        }
+
+        private void DeleteStation_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(EraseIcon, "delete", DragDropEffects.Move);
+        }
+
+        private void ToggleButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //StationsNetworkMap.SetView();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            StackPanel panel = grid.Children[1] as StackPanel;
+            TextBlock textBlock = panel.Children[0] as TextBlock;
+
+            Station station = GetStationByName(textBlock.Text);
+            StationsNetworkMap.Center = station.Location;
+            StationsNetworkMap.Focus();
+            StationsNetworkMap.ZoomLevel = 15;
+        }
+
+        public Station GetStationByName(String name)
+        {
+            foreach (Station station in Stations)
+                if (station.Name.Equals(name)) return station;
+            return null;
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            StackPanel panel = grid.Children[1] as StackPanel;
+            TextBlock textBlock = panel.Children[0] as TextBlock;
+
+            Station station = GetStationByName(textBlock.Text);
+            StationsNetworkMap.Center = station.Location;
+            StationsNetworkMap.Focus();
+            StationsNetworkMap.ZoomLevel = 15;
         }
     }
 
