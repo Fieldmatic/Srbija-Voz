f62d6ea946ea6607d5e65dce1a92f5171b5e3d3a
banzg00 <63750104+banzg00@users.noreply.github.com>
2022-06-12 02:38:35 +0200

    Rijesen bag sa dupliranjem stanica

--- SrbijaVoz/SrbijaVoz/managerWindows/line/CreateLine.xaml.cs
+++ SrbijaVoz/SrbijaVoz/managerWindows/line/CreateLine.xaml.cs
@@ -66,13 +66,17 @@ namespace SrbijaVoz.managerWindows
                 ListViewItem listViewItem =
                     FindAncestor<ListViewItem>((DependencyObject)e.OriginalSource);
 
-                // Find the data behind the ListViewItem
-                Station station = (Station)listView.ItemContainerGenerator.
+                try
+                {
+                    // Find the data behind the ListViewItem
+                    Station station = (Station)listView.ItemContainerGenerator.
                     ItemFromContainer(listViewItem);
+                    // Initialize the drag & drop operation
+                    DataObject dragData = new DataObject("myFormat", station);
+                    DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+                }
+                catch (ArgumentNullException) { }
 
-                // Initialize the drag & drop operation
-                DataObject dragData = new DataObject("myFormat", station);
-                DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
             }
         }
 
@@ -103,6 +107,8 @@ namespace SrbijaVoz.managerWindows
             if (e.Data.GetDataPresent("myFormat"))
             {
                 Station station = e.Data.GetData("myFormat") as Station;
+                if (SettedStations.Contains(station))
+                    return;
                 AvailableStations.Remove(station);
                 SettedStations.Add(station);
             }
@@ -113,6 +119,8 @@ namespace SrbijaVoz.managerWindows
             if (e.Data.GetDataPresent("myFormat"))
             {
                 Station station = e.Data.GetData("myFormat") as Station;
+                if (AvailableStations.Contains(station))
+                    return;
                 SettedStations.Remove(station);
                 AvailableStations.Add(station);
             }
